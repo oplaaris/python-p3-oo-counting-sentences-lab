0c3467a413b1d2cd67e909eb2d0292689c8ac3e7
Victor Leina Ngeny <victorleinangeny@gmail.com>
2023-06-03 15:24:48 +0300

    lab attempted

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value):
+        if isinstance(value, str):
+            self.value = value
+        else:
+            raise ValueError("Value must be a string.")
+
+    def is_sentence(self):
+        return self.value.endswith(".")
+
+    def is_question(self):
+        return self.value.endswith("?")
+
+    def is_exclamation(self):
+        return self.value.endswith("!")
+
+    def count_sentences(self):
+        sentences = self.value.split(".")
+
+        sentences = [sentence for sentence in sentences if sentence.strip()]
+        return len(sentences)
